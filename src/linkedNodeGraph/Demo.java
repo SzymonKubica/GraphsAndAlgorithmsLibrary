f9eab994ed72f10d3e9c55bd03f07ef685dc6f68
SzymonKubica <szymo.kubica@gmail.com>
2021-04-02 22:17:24 +0200

    Implemented and tested Prim's algorithm for MST.

--- src/linkedNodeGraph/Demo.java
+++ src/linkedNodeGraph/Demo.java
@@ -2,6 +2,26 @@ package linkedNodeGraph;
 
 public class Demo {
   public static void main(String[] args) {
+    WeightedGraph<Integer> test = new WeightedGraph<>();
+
+    Node<Integer> node1 = new Node<>(1, test);
+    Node<Integer> node2 = new Node<>(2, test);
+    Node<Integer> node3 = new Node<>(3, test);
+    Node<Integer> node4 = new Node<>(4, test);
+
+    test.addEdgeWithWeight(node1, node2, 3);
+    test.addEdgeWithWeight(node2, node3, 4);
+    test.addEdgeWithWeight(node3, node4, 2);
+    test.addEdgeWithWeight(node4, node1, 4);
+    test.addEdgeWithWeight(node1, node3, 5);
+
+
+
+    Tree<Integer> minimumSpanningTree = test.MST(node1);
+    System.out.println(GraphAlgorithm.BFS(minimumSpanningTree.root));
+    // TODO: Add MST algorithm to GraphAlgorithms class.
+    // TODO: Migrate this MST test into the test suite.
+
 
   }
 
--- src/linkedNodeGraph/WeightedGraph.java
+++ src/linkedNodeGraph/WeightedGraph.java
@@ -1,8 +1,6 @@
 package linkedNodeGraph;
 
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class WeightedGraph<T> extends UndirectedGraph<T> {
@@ -15,50 +13,71 @@ public class WeightedGraph<T> extends UndirectedGraph<T> {
 
   public void addEdgeWithWeight(Node<T> origin, Node<T> destination, int weight) {
     super.addEdge(origin, destination);
+    // Edges are undirected
     WeightedEdge<T> edge = new WeightedEdge<>(origin, destination, weight);
+    WeightedEdge<T> edge2 = new WeightedEdge<>(destination, origin, weight);
     edges.add(edge);
+    edges.add(edge2);
   }
 
 
   public Set<WeightedEdge<T>> getEdgesStartingAt(Node<T> origin) {
-    return edges.stream().filter(edge -> edge.getOrigin() == origin).collect(Collectors.toSet());
+    return edges.stream().filter(edge -> edge.getOrigin().equals(origin)).collect(Collectors.toSet());
+  }
+
+  public Set<WeightedEdge<T>> getEdgesStartingInTheTreeEndingAt(Node<T> destination, Tree<T> mst) {
+    return edges.stream().filter(edge -> edge.getDestination().equals(destination) && mst.nodes.contains(edge.getOrigin()))
+            .collect(Collectors.toSet());
+  }
+
+  private int getEdgeWeight(Node<T> origin, Node<T> destination) {
+    return getEdgesStartingAt(origin).stream()
+            .filter(edge -> edge.getDestination().equals(destination))
+            .mapToInt(WeightedEdge::getWeight).boxed().collect(Collectors.toList()).get(0);
+  }
+
+  private WeightedEdge<T> getMinimumEdgeEndingAt(Node<T> destination, Map<Node<T>, Integer> startingNodeWeightMap, Tree<T> mst) {
+    Set<WeightedEdge<T>> edgesEndingAtDestination = getEdgesStartingInTheTreeEndingAt(destination, mst);
+    return edgesEndingAtDestination.stream()
+            .min(Comparator.comparingInt(WeightedEdge::getWeight)).get();
   }
 
   public Tree<T> MST(Node<T> start) {
     Tree<T> minST = new Tree<>();
+    Map<Node<T>, Integer> weightOfNodesMap = new HashMap<>();
+    weightOfNodesMap.put(start, 0);
     minST.root = start;
+    minST.nodes.add(start);
     Set<Node<T>> fringe = new HashSet<>();
-    fringe.addAll(start.adjacentNodes);
+    for (Node<T> neighbour : start.adjacentNodes) {
+      fringe.add(neighbour);
+      weightOfNodesMap.put(neighbour, getEdgeWeight(start, neighbour));
+    }
     while (!fringe.isEmpty()) {
-      WeightedEdge<T> shortest = getMinimumWeightEdge(minST, fringe);
-      Node<T> closest = getClosestNode(minST, fringe);
-      minST.addNode(closest);
-      minST.addEdge(shortest);
-      for (Node<T> neighbour : closest.adjacentNodes) {
-        if (!fringe.contains(neighbour)) {
-          fringe.add(neighbour);
+      Node<T> closestNode = getMinimumWeightNode(fringe, weightOfNodesMap);
+      WeightedEdge<T> minWeightEdge = getMinimumEdgeEndingAt(closestNode, weightOfNodesMap, minST);
+      minST.addNode(closestNode);
+      fringe.remove(closestNode);
+      minST.addEdge(minWeightEdge);
+      for (Node<T> neighbour : closestNode.adjacentNodes) {
+        if (!minST.nodes.contains(neighbour)) {
+          if (fringe.contains(neighbour)) {
+            // Updating candidate arc.
+            if (getEdgeWeight(closestNode, neighbour) < weightOfNodesMap.get(neighbour)) {
+              weightOfNodesMap.put(neighbour, getEdgeWeight(closestNode, neighbour));
+            }
+          } else {
+            // neighbour is unseen.
+            fringe.add(neighbour);
+            weightOfNodesMap.put(neighbour, getEdgeWeight(closestNode, neighbour));
+          }
         }
       }
     }
     return minST;
   }
 
-  private WeightedEdge<T> getMinimumWeightEdge(Tree<T> mst, Set<Node<T>> fringe) {
-    return edges.stream().filter(edge -> connectsTreeWithFringe(edge, mst, fringe))
-            .min(Comparator.comparingInt(WeightedEdge::getWeight)).get();
-  }
-
-  private Node<T> getClosestNode(Tree<T> mst, Set<Node<T>> fringe) {
-    WeightedEdge<T> shortest = getMinimumWeightEdge(mst, fringe);
-    if (fringe.contains(shortest.getDestination())) {
-      return shortest.getDestination();
-    } else {
-      return shortest.getOrigin();
-    }
-  }
-
-  private boolean connectsTreeWithFringe(WeightedEdge<T> edge, Tree<T> mst, Set<Node<T>> fringe) {
-    return mst.nodes.contains(edge.getOrigin()) && fringe.contains(edge.getDestination())
-            || mst.nodes.contains(edge.getDestination()) && fringe.contains(edge.getOrigin());
+  private Node<T> getMinimumWeightNode(Set<Node<T>> fringe, Map<Node<T>, Integer> weightOfNodesMap) {
+    return fringe.stream().min(Comparator.comparingInt(node -> weightOfNodesMap.get(node))).get();
   }
 }
